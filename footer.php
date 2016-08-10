0c17c48c4a01937718a96cf41e911f226fda996a
katerinakiseleva <kate.kiseleva@mail.utoronto.ca>
2016-08-09 21:51:30 -0400

    work

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package blackpearl
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'blackpearl' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'blackpearl' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'blackpearl' ), 'blackpearl', '<a href="http://underscores.me/" rel="designer">Ekaterina Kiseleva</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
