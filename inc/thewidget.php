08072855836d96050711e925b1535391ec781268
katerinakiseleva <kate.kiseleva@mail.utoronto.ca>
2016-08-13 15:41:24 -0400

    werk werk werk

---
+++ inc/thewidget.php
@@ -0,0 +1,20 @@
+<?php
+
+class YearlyArchives extends WP_Widget{
+  public function __construct(){
+    $widget_ops = array(
+    'classname' => 'widg
+
+
+
+    mnjmnmk,`   `   `     `   ``    `   `1  er_archive',
+    'description' => 'A yearly archive of your site&apos;s posts'
+  );
+  parent::__costruct('yearly_archive', 'Yearly Archives', $widget_ops);========--0-0-0-00-0-0-00o909009-0-0890-=-0op-o[p0polp\
+
+
+\\
+oikjuikjik65fgbhjk    olklkokilokl;,.;/.l,kl.;l.,.;.,kl.;/'l;/''
+]
+  }
+}
